bdb7b8c5d86d2a556d5e9e2d416327cf1fa9a487
Calvin See <calvinfrancissee@gmail.com>
2022-03-31 14:42:05 -0700

    Added buttons and styled the related products list to be centered on the page

--- src/components/related/ProductCard.jsx
+++ src/components/related/ProductCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = (props) => {
   return (
     <div className="product-card">
-      <h1></h1>
+      <h1>{props.num}</h1>
     </div>
   );
 };
--- src/components/related/RelatedProducts.jsx
+++ src/components/related/RelatedProducts.jsx
@@ -5,8 +5,15 @@ const RelatedProducts = () => {
 
   return (
     <div className="related-products">
-      <div className="related-product-carousel">
-        {[1,2,3,4,5,6,7,8,9,10].map((num) => <ProductCard key={num} />)}
+      <span>Related Products</span>
+      <div className="carousel-container">
+        <button className="arrow">Left</button>
+        <div className="carousel-viewport">
+          <div className="carousel">
+            {[1,2,3,4,5,6,7,8,9,10].map((num) => <ProductCard num={num} key={num} />)}
+          </div>
+        </div>
+        <button className="arrow">Right</button>
       </div>
     </div>
   );
