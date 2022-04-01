d5995c5d87ccbcf8eb8a13b0d7ea7ddadd15cff0
Calvin See <calvinfrancissee@gmail.com>
2022-04-01 14:06:16 -0700

    imported axios and created authorization + url variables

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,15 @@ import Overview from "./overview/Overview.jsx";
 import ProductLists from "./related/ProductLists.jsx";
 import Ratings from "./ratings/Ratings.jsx";
 import config from '/config.js';
+import axios from 'axios';
 
 const App = () => {
+
+  const header = {
+    'Authorization': `token ${config.TOKEN}`
+  };
+  const host = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
+
   return (
     <div>
       <CSSStyles />
