f801a105c26fb5ffcfa6173e815257ccafbd501e
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-01 16:46:23 -0700

    Adding API functionality!

--- src/components/overview/ImageCarousel.jsx
+++ src/components/overview/ImageCarousel.jsx
@@ -1,16 +1,28 @@
-import React, { useRef } from "react";
-import { ImageData } from "./dummyImages.js";
+import React, { useState, useRef, useEffect } from "react";
+// import { ImageData } from "./dummyImages.js";
 import Image from "./Image.jsx";
+import {fetchProducts} from "../../helpers.js";
+
 
 const ImageCarousel = () => {
-
   const imageCarouselRef = useRef(null);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => {
+        setImages(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
 
   return (
     <div className="overview-images">
       <ul ref={imageCarouselRef} className="image-carousel">
-        {ImageData.map(image =>
-          <Image key={image} slide={image}/>
+        {images.map(image =>
+          <Image key={image.id} slide={image}/>
         )}
       </ul>
     </div>
--- src/components/overview/ProductInformation.jsx
+++ src/components/overview/ProductInformation.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StyleSelector from './StyleSelector.jsx';
+import {fetchProducts} from "../../helpers.js";
 
 const ProductInformation = () => {
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => {
+        setProduct(response[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
+  console.log(product);
 
   return (
     <div className="overview-product-info">
-      <h1>Expanded Product Name</h1>
+      <h1>{product.name}</h1>
+      <section className="body-text">{product.description}</section>
       <section className="body-text">$369</section>
       <section className="body-text">STYLE</section>
       <StyleSelector />
