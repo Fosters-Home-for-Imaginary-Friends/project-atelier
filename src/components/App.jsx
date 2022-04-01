ebe5094d62a74bf56114be2b905571a9d1b7d9a4
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-01 11:28:08 -0700

    Completed base model for product information!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Styles from "./Styles.jsx";
+import CSSStyles from "./CSSStyles.jsx";
 import Overview from "./overview/Overview.jsx";
 import ProductLists from "./related/ProductLists.jsx";
 
 const App = () => {
   return (
     <div>
-      <Styles />
+      <CSSStyles />
       <Overview />
       <ProductLists />
     </div>
---
+++ src/components/overview/Style.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Style = () => {
+  return (
+    <div className="style">
+      Style
+    </div>
+  )
+}
+
+export default Style;
