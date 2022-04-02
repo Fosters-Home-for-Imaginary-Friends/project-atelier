7b21c7ab2744483ff81f53dcaf5239f857789d2d
Michael Washburn <m.washburn@mail.com>
2022-04-02 13:15:37 -0700

    added file for average star display feature.

---
+++ src/components/ratings/AverageStars.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+let AverageStars = (props) => {
+
+//temp variable to progress while api call having issues with timing
+let averageRating = 3.7;
+
+//create an array to store stars from average rating
+let numOfStars = [];
+//push an element for each whole star in the rating
+while (numOfStars.length < 5 ) {
+  if ( averageRating > 1) {
+    numOfStars.push(1);
+    //once below 1 total star, create values for each quarter star
+  } else if ( averageRating > 0) {
+    let zero = Math.abs(0 - rating);
+    let oneQuarter = Math.abs(.25 - rating);
+    let half = Math.abs(.5 - rating);
+    let threeQuarters = Math.abs(.75 - rating);
+    let max = Math.abs(1 - rating);
+    //find the closest quarter star
+    let closestPercent = Math.min(zero, oneQuarter, half, threeQuarters, max);
+    // create a switch statement for each option
+    switch (closestPercent) {
+        case(zero):
+      numOfStars.push(0);
+      break;
+        case(oneQuarter):
+      numOfStars.push(.25);
+      break;
+        case(half):
+      numOfStars.push(.5);
+      break;
+        case(threeQuarters):
+      numOfStars.push(.75);
+      break;
+        case(max):
+      numOfStars.push(1);
+      break;
+        default:
+      console.log('How did this happen');
+      numOfStars.push(0);
+      break;
+
+    }
+  } else {
+    numOfStars.push(0);
+  }
+  averageRating = averageRating - 1
+}
+
+  return (
+    <div>
+      {numOfStars.map((star, i) => {
+        return (
+          <div className="single-star-container" key={i}>
+            <div className="single-star-fill" style={{"width" : `${parseInt(star * 30)}px`}}>
+
+
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default AverageStars;
