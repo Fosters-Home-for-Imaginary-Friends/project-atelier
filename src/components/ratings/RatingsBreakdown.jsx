805cb8996622bed7c74c30c5eab16f938406120c
Michael Washburn <m.washburn@mail.com>
2022-04-02 13:14:47 -0700

    added function to calculate average rating from metaData call. ***FUNCTION RUNNING BEFORE API CALL FINISHES***

--- src/components/ratings/RatingsBreakdown.jsx
+++ src/components/ratings/RatingsBreakdown.jsx
@@ -1,12 +1,30 @@
 //Component for the ratings breakdown
 import React from 'react';
+import AverageStars from './AverageStars.jsx';
 
-let RatingsBreakdown = () => {
+let RatingsBreakdown = (props) => {
+console.log(props.metaRating);
 
+//having trouble with render firing before api calls, storing var to hold average rating while trying to fix
+let averageStar = 3.7;
+
+//function to calculate the average rating for the product based on total ratings
+let averageRating = (ratingObj) => {
+  console.log(ratingObj);
+  let totalVotes = 0;
+  let totalScore = 0;
+  for ( let k in ratingObj) {
+    totalVotes += ratingObj[k];
+    totalScore += (ratingObj[k] * parseInt(k));
+    return (totalScore / totalVotes);
+  }
+}
 
   return (
     <div className= "rating-breakdown-container">
       <h3 className= "rating-breakdown-title"> Ratings and Reviews</h3>
+      <h1> {averageStar} </h1>
+      <AverageStars ratingsObj= {props.metaRating.ratings}/>
     </div>
   );
 };
