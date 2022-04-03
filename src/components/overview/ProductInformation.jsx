af1dc56e73248ffc7eedb7a7338a263aae8fc427
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-03 16:27:19 -0700

    Added scalable functionality to overview component!

--- src/components/overview/ProductInformation.jsx
+++ src/components/overview/ProductInformation.jsx
@@ -7,25 +7,11 @@ const ProductInformation = ({ product }) => {
     <div className="overview-product-info">
       <h1>{product.name}</h1>
       <section className="body-text">{product.description}</section>
-      <section className="body-text">{product.default_price}</section>
-      <section className="body-text">STYLE</section>
+      <section className="body-text">{product.default_price} USD</section>
       <StyleSelector productId={product.id}/>
       <div>
         <button className="info-button add">ADD TO BAG</button>
       </div>
-      {/* <h1>Product Name</h1>
-      <section className="body-text">Product Description</section>
-      <section className="body-text">PRICE</section>
-      <section className="body-text">STYLE</section>
-      <StyleSelector />
-      <div>
-        <button className="info-button">SELECT SIZE</button>
-        <button className="info-button">1</button>
-      </div>
-      <div>
-        <button className="info-button">ADD TO BAG</button>
-        <button className="info-button">*</button>
-      </div> */}
     </div>
   )
 }
--- src/components/overview/Style.jsx
+++ src/components/overview/Style.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Style = ({style}) => {
-  console.log(style);
   var colors = style.name.split(' & ');
 
   for (let i = 0; i < colors.length; i++) {
@@ -12,10 +11,10 @@ const Style = ({style}) => {
       colors[i] = colors[i].toLowerCase();
     }
   }
-  console.log(colors);
+
   var side1 = `style-color side1 ${colors[0]}`;
   var side2 = `style-color side2 ${colors[1]}`;
-  console.log(side1);
+
   return (
     <div className="style-buttons">
       {/* <img src={style.photos[0].thumbnail_url} className="style"/>
