469a747662bb08db404086ed06412f74f351ddac
Andrew Tran <amtran150@gmail.com>
2022-04-04 12:00:25 -0700

    rendered the widget onto the webpage

--- src/components/qna/Qna.jsx
+++ src/components/qna/Qna.jsx
@@ -1,15 +1,18 @@
-// import React from 'react';
+import React from 'react';
+import data from './data.js';
+import QuestionsList from './QuestionsList.jsx';
 
-// const Qna = (props) => {
-//   return (
-//     <div>
-//         <h2>Questions and Answers</h2>
-//       Search Bar
-//       Questions List
-//       See more questions button
-//       Add a question button
-//     </div>
-//   );
-// }
+const Qna = (props) => {
+  return (
+    <div className="qna-container">
+        <h3>Questions and Answers</h3>
+      {/* Search Bar */}
+      {/* Questions List */}
+      <QuestionsList data={data.results}/>
+      {/* See more questions button */}
+      {/* Add a question button */}
+    </div>
+  );
+}
 
-// export default Qna;
+export default Qna;
--- src/components/qna/QuestionsList.jsx
+++ src/components/qna/QuestionsList.jsx
@@ -1,9 +1,14 @@
-// import React from 'react';
+import React from 'react';
+import Question from './Question.jsx';
 
-// const QuestionsList = (props) => {
-//   return(
+const QuestionsList = (props) => {
+  return(
+    <div>
+      {props.data.map((product) =>
+        <Question data={product} key={product.question_id}/>
+      )}
+    </div>
+  );
+}
 
-//   );
-// }
-
-// export default QuestionsList;
+export default QuestionsList;
