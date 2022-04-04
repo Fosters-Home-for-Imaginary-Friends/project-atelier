d0f4a52b007e83dfe6ef2be546ddc2e54119c3ff
Calvin See <calvinfrancissee@gmail.com>
2022-04-04 11:37:10 -0700

    Product cards now take in api data

--- src/components/related/ProductCard.jsx
+++ src/components/related/ProductCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const ProductCard = (props) => {
+const ProductCard = ({product}) => {
   return (
     <div className="product-card">
       <div className="related-image">
-        <h1>{props.num}</h1>
+        <h1>Card</h1>
       </div>
       <div className="related-info">
-        <span className="related-category">Category</span><br />
-        <span className="related-name">Product Name</span><br />
-        <span className="related-price">$99999</span> <br />
+        <span className="related-category">{product.category}</span><br />
+        <span className="related-name">{product.name}</span><br />
+        <span className="related-price">{product.default_price}</span> <br />
         <span className="related-rating">stars</span>
       </div>
 
