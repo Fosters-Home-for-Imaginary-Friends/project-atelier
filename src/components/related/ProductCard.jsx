9b74bd44ff4742490a489c859c8da9dc530f85df
Calvin See <calvinfrancissee@gmail.com>
2022-04-04 10:39:05 -0700

    Created info divs

--- src/components/related/ProductCard.jsx
+++ src/components/related/ProductCard.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const ProductCard = (props) => {
   return (
     <div className="product-card">
-      <h1>{props.num}</h1>
+      <div className="related-image">
+        <h1>{props.num}</h1>
+      </div>
+      <div className="related-info">
+        <span className="related-category">Category</span><br />
+        <span className="related-name">Product Name</span><br />
+        <span className="related-price">$99999</span> <br />
+        <span className="related-rating">stars</span>
+      </div>
+
     </div>
   );
 };
