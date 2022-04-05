917a823b4ed123786c88003422c0786902be5319
Calvin See <calvinfrancissee@gmail.com>
2022-04-04 17:46:55 -0700

    Added star to product card

--- src/components/related/ProductCard.jsx
+++ src/components/related/ProductCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {AiOutlineStar} from 'react-icons/ai';
 
 const ProductCard = ({product, styles}) => {
+
   return (
     <div className="product-card">
       <img className="related-image" src={styles[0].photos[0].url} />
+      <AiOutlineStar />
       <div className="related-info">
         <span className="related-category">{product.category}</span><br />
         <span className="related-name">{product.name}</span><br />
