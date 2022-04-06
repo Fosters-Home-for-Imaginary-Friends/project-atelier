23f02bcaa2f4224ade7f64b459ea295b9202c0c1
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-05 20:49:17 -0700

    Update overview_test.js

--- cypress/integration/overview_test.js
+++ cypress/integration/overview_test.js
@@ -2,6 +2,6 @@ describe('My First Test', () => {
   it('Visits Atelier website', () => {
     cy.visit('http://localhost:3000')
 
-    cy.contains('Camo Onesie')
+    cy.contains('ADD TO BAG')
   })
 })
