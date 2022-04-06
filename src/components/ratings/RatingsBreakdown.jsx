d6ce9f8eb9ea99f2ee0b77a7a63a2b3e9a03e98d
Michael Washburn <m.washburn@mail.com>
2022-04-06 09:20:29 -0700

    added some graph functionality, needs sizing and second graphs

--- src/components/ratings/RatingsBreakdown.jsx
+++ src/components/ratings/RatingsBreakdown.jsx
@@ -1,19 +1,46 @@
 //Component for the ratings breakdown
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AverageStars from './AverageStars.jsx';
+import { fetchReviewMetadata } from '../../helpers.js';
+import BarGraph from './BarGraph.jsx';
 
 let RatingsBreakdown = (props) => {
+
+  const [loading, setLoading] = useState(true);
+  const [metaRatings, setMetaRatings] = useState({})
+
+  useEffect(() => {
+    fetchReviewMetadata(40384).then(res => {
+      setMetaRatings(res);
+      setLoading(false);
+    }).catch(err => {
+      console.error(err);
+    })
+  }, []);
 
 //having trouble with render firing before api calls, storing var to hold average rating while trying to fix
 let averageStar = 3.7;
 
 
+  if ( loading ) {
+    return (
+      <div>
+        Loading. . .
+      </div>
+    )
+  }
+
 
   return (
+
     <div className= "rating-breakdown-container">
       <h3 className= "rating-breakdown-title"> Ratings and Reviews</h3>
-      <h1> {averageStar} </h1>
-      <AverageStars ratingsObj= {props.metaRating.ratings}/>
+      <div className="ratings-breakdown-avg-stars-container">
+        <AverageStars ratingsObj= {props.metaRating.ratings}/>
+      </div>
+      <div className="bar-graph-container">
+        <BarGraph />
+      </div>
     </div>
   );
 };
