fc29e2827ac3279f93119dfa6e1fc87c6dc6178c
Calvin See <calvinfrancissee@gmail.com>
2022-04-06 15:02:46 -0700

    Created Cookie Helper Functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,19 @@ import Lists from "./related/Lists.jsx";
 import Qna from "./qna/Qna.jsx";
 import Ratings from "./ratings/Ratings.jsx";
 import {currentPlaceholder} from './related/relatedHelpers.js';
+import {createCookie, getAllCookies, deleteCookie} from '../cookies.js';
+
+
 
 
 const App = () => {
+
+  createCookie("object", JSON.stringify({product: 158923}));
+  console.log("cookies: ", getAllCookies());
+  console.log("parsed cookie: ", JSON.parse(getAllCookies()));
+  deleteCookie("object");
+  console.log("cookies: ", getAllCookies());
+
 
   return (
     <div>
