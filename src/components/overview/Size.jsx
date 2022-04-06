d5ad3b3a602d06a957f6007cbaaa50203e0190c4
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-06 10:16:51 -0700

    Added state to Size component!

--- src/components/overview/Size.jsx
+++ src/components/overview/Size.jsx
@@ -1,13 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import { OverviewContext } from "./Overview.jsx";
 
 const Size = ({sizes}) => {
 
   const { setCurrentSize } = useContext(OverviewContext);
+  const [current, setCurrent] = useState(false);
+
+  const setCurrentSizing = () => {
+    setCurrentSize(sizes);
+    setCurrent(!current);
+  }
 
   return (
-    <div className="size-button" onClick={() => setCurrentSize(sizes)}>
+    <div className={`size-button ${current}`} onClick={() => setCurrentSizing()}>
       <span className="size-text">{sizes.size}</span>
     </div>
   )
