d8014f1d67f315229aa37cc42e5a2b47f4a7f319
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-07 09:53:05 -0700

    Added global state for product id!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import CSSStyles from "./CSSStyles.jsx";
 import Overview from "./overview/Overview.jsx";
 import RelatedProducts from "./related/RelatedProducts.jsx";
@@ -7,17 +7,23 @@ import Qna from "./qna/Qna.jsx";
 import Ratings from "./ratings/Ratings.jsx";
 import {currentPlaceholder} from './related/RelatedHelpers.js';
 
+export const AppContext = createContext({});
+
 const App = () => {
 
+  const [productId, setProductId] = useState(40344);
+
   return (
-    <div>
-      <Overview />
-      <RelatedProducts current={currentPlaceholder} />
-      <OutfitList current={currentPlaceholder} />
-      <Qna />
-      <Ratings />
-      <CSSStyles />
-    </div>
+    <AppContext.Provider value={{ productId, setProductId }}>
+      <div>
+        <Overview />
+        <RelatedProducts current={currentPlaceholder} />
+        <OutfitList current={currentPlaceholder} />
+        <Qna />
+        <Ratings />
+        <CSSStyles />
+      </div>
+    </AppContext.Provider>
   );
 }
 
