91aba6ecd4caa3fd970e69d0b8c387e6120a6029
Calvin See <calvinfrancissee@gmail.com>
2022-04-07 14:35:00 -0700

    Decoupled Outfit List

--- src/components/related/CardCarousel.jsx
+++ src/components/related/CardCarousel.jsx
@@ -1,6 +1,7 @@
-import React, {useRef, useEffect, useState} from 'react';
+import React, {useRef, useEffect, useState, useMemo} from 'react';
 import {getRelated} from '../../helpers.js';
 import {ProductCard, AddProductCard} from './ProductCards.jsx';
+import {getCookie} from './RelatedHelpers.js';
 
 const CardCarousel = ({product_id, related}) => {
   const carouselRef = useRef(null);
@@ -23,7 +24,7 @@ const CardCarousel = ({product_id, related}) => {
     <div className="carousel-container" id="modal"> {/* This holds the carousel viewport and the buttons */}
       <button onClick={scrollLeft} className="arrow">Left</button>
       <div ref={carouselRef} className="carousel-viewport"> {/* The portion of the carousel that is visible to the user */}
-        {related ? <RelatedCards product_id={product_id} /> : null}
+        {related ? <RelatedCards product_id={product_id} /> : <OutfitCards product_id={product_id} />}
       </div>
       <button onClick={scrollRight} className="arrow">Right</button>
     </div>
@@ -31,22 +32,38 @@ const CardCarousel = ({product_id, related}) => {
 };
 
 const RelatedCards = React.memo(function RelatedCards({product_id}) {
-  const idList = useRef([]);
-  const [loadState, setLoadState] = useState(false);
+  const [relatedList, setRelatedList] = useState([]);
 
   useEffect(() => {
     getRelated(product_id)
       .then((data) => {
-        idList.current = data;
-        setLoadState(true);
+        setRelatedList(data);
       })
       .catch((err) => console.error(err));
   }, [product_id]);
 
   return (
   <div className="carousel"> {/* The part that scrolls when you press a button */}
-    {loadState ? idList.current.map((id) => <ProductCard key={id} product_id={id} related={true} />) : null}
+    {relatedList.map((id) => <ProductCard key={id} product_id={id} related={true} />)}
   </div>
+  );
+});
+
+const OutfitCards = React.memo(function OutfitCards({product_id}) {
+  const [outfitList, setOutfitList] = useState([]);
+
+  useEffect(() => {
+    let cookies = getCookie("outfitList");
+    if (cookies !== "") {
+      setOutfitList((list) => list.concat(JSON.parse(cookies)))
+    }
+  }, []);
+
+  return (
+    <div className="carousel"> {/* The part that scrolls when you press a button */}
+      {[<AddProductCard product_id={product_id} key={"addproductcard"} setOutfitList={setOutfitList} />]
+      .concat(outfitList.map((id) => <ProductCard key={id} related={false} />))}
+    </div>
   );
 });
 
--- src/components/related/OutfitList.jsx
+++ src/components/related/OutfitList.jsx
@@ -1,33 +1,19 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React from 'react';
 import CardCarousel from './CardCarousel.jsx';
-import {getAllCookies, getCookie} from '../../Cookies.js';
+import {getAllCookies} from '../../Cookies.js';
 
 const OutfitList = ({product_id}) => {
   // const [cardData, setCardData] = useState([]);
   //{card: <ProductCard />, productData: getRelatedProductsData()}
   // TODO: optimize map function
   // TODO: read about browser cookie size limits
-  const outfitList = useRef(getCookie("outfitList"));
-
-  // useEffect(() => {
-  //   const outfitList = getCookie("outfitList");
-  //   if (outfitList !== "") {
-  //   }
-  // }, []);
-        // getOutfitListData(JSON.parse(outfitList))
-      //   .then((productData) => setCardData(() => {
-      //     return productData.map((product) => ({card: (<ProductCard setCardData={setCardData}
-      //       card={product} current={current} key={generateKey()}
-      //       related={false} />), productData: product}));
-      //   }))
-      //   .catch((err) => console.error(err));
 
   console.log("Outfit List: ", getAllCookies());
 
   return (
     <div className="product-list" id="outfit-list">
       <span>Your Outfit</span>
-      <CardCarousel />
+      <CardCarousel product_id={product_id} />
     </div>
   );
 };
