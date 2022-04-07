3de05342240c0136f77c66900d068ef5cc455b76
Calvin See <calvinfrancissee@gmail.com>
2022-04-07 16:18:53 -0700

    Imported functions to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,21 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useMemo, useEffect } from "react";
 import CSSStyles from "./CSSStyles.jsx";
 import Overview from "./overview/Overview.jsx";
 import RelatedProducts from "./related/RelatedProducts.jsx";
 import OutfitList from './related/OutfitList.jsx';
 import Qna from "./qna/Qna.jsx";
 import Ratings from "./ratings/Ratings.jsx";
+import {getProduct} from '../helpers.js';
 
 export const AppContext = createContext({});
 
 const App = () => {
 
   const [productId, setProductId] = useState(40344);
+  const appContextValue = useMemo(() => ({productId, setProductId}), [productId, setProductId]);
 
   return (
-    <AppContext.Provider value={{ productId, setProductId }}>
+    <AppContext.Provider value={appContextValue}>
       <div>
         <Overview />
         <RelatedProducts product_id={40344} />
