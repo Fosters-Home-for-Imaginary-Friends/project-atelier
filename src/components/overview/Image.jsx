3cc674bded6068fa27568804b4bb09d8df9be052
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-08 16:40:05 -0700

    Completed Modal!

--- src/components/overview/Image.jsx
+++ src/components/overview/Image.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useLayoutEffect, useRef } from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { OverviewContext } from './Overview.jsx';
 
@@ -6,33 +6,16 @@ const Image = (props) => {
 
   const { slide } = props;
 
-  const { setShowModal, currentPhoto, loading } = useContext(OverviewContext);
+  const { setShowModal } = useContext(OverviewContext);
 
-  const imageRef = useRef(null);
-
-  // const [imageWidth, setImageWidth] = useState({width: 200, height: 200});
-
-  // useLayoutEffect(() => {
-  //   if (!loading) {
-  //     console.log('Im here!')
-  //     setImageWidth({
-  //       width: imageRef.current.offsetWidth,
-  //       height: imageRef.current.offsetHeight
-  //     })
-  //   }
-  // }, [loading]);
-
-  // const handleViewChange = (e) => {
-  //   console.log(e.target.width)
-  //   setImageWidth({
-  //     width: 500,
-  //     height: e.target.height
-  //   })
-  // }
+  const handleClick = () => {
+    document.body.classList.add('modal-open');
+    setShowModal(true);
+  }
 
   return (
     <li>
-      <img src={slide.url} key={slide.image} ref={imageRef} className="image" onClick={() => setShowModal(true)}/>
+      <img src={slide.url} key={slide.image} className="image" onClick={() => handleClick()}/>
     </li>
   )
 }
--- src/components/overview/ImageModal.jsx
+++ src/components/overview/ImageModal.jsx
@@ -1,17 +1,47 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useState, useLayoutEffect, useEffect } from "react";
 import ReactDom from "react-dom";
 import { OverviewContext } from "./Overview.jsx";
 
 const ImageModal = () => {
 
-  const imageModalRef = useRef();
+  const imageModalRef = useRef(null);
 
-  const { setShowModal, currentStyle, currentPhoto } = useContext(OverviewContext);
+  const { setShowModal, currentStyle, currentPhoto, loading } = useContext(OverviewContext);
+  const [top, setTop] = useState(0);
+  const [yPosition, setYPosition] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const handlePosition = (e) => {
+    const container = imageModalRef.current.getBoundingClientRect();
+    setYPosition(e.clientY * (((height * 2) - e.clientY) / container.height))
+  }
+
+  if (imageModalRef.current && !height) {
+    setHeight(imageModalRef.current.scrollHeight)
+  }
+  useLayoutEffect(() => {
+    setHeight(imageModalRef.current.scrollHeight)
+  }, [loading, setShowModal])
+
+  useEffect(() => {
+    setTop(-yPosition)
+  }, [yPosition])
+
+  const handleClick = () => {
+    document.body.classList.remove('modal-open')
+    setShowModal(false);
+  }
 
   return ReactDom.createPortal(
-    <div className="image-modal-container" ref={imageModalRef}>
+    <div id="image-modal-container" className="image-modal-container" ref={imageModalRef}>
       <div className="image-modal">
-        <img src={currentStyle.photos[currentPhoto].url} onClick={() => {setShowModal(false)}} className="image-modal-image"/>
+        <img
+          src={currentStyle.photos[currentPhoto].url}
+          onClick={() => handleClick()} className="image-modal-image"
+          // onMouseOver={(e) => handlePosition(e)}
+          onMouseMove={(e) => handlePosition(e)}
+          style = {{top}}
+        />
       </div>
     </div>,
     document.getElementById("imageModal")
