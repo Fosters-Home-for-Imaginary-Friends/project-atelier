ee776b9750186d985bcce9190b46ada41814f3a4
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-11 15:26:46 -0700

    Completed modal functionality to allow user to flip through photos in modal view!

--- src/components/overview/ImageModal.jsx
+++ src/components/overview/ImageModal.jsx
@@ -7,7 +7,7 @@ const ImageModal = () => {
   const imageModalRef = useRef(null);
   const imageRef = useRef(null);
 
-  const { setShowModal, currentStyle, currentPhoto, loading } = useContext(OverviewContext);
+  const { setShowModal, currentStyle, setPreviousPhoto, currentPhoto, setCurrentPhoto, loading } = useContext(OverviewContext);
   const [top, setTop] = useState(0);
   const [yPosition, setYPosition] = useState(0);
   const [height, setHeight] = useState(0);
@@ -23,23 +23,51 @@ const ImageModal = () => {
 
   useLayoutEffect(() => {
     setHeight(imageRef.current.scrollHeight)
-  }, [loading, setShowModal])
+  }, [loading, setShowModal, currentPhoto])
 
   useEffect(() => {
     setTop(-yPosition)
-  }, [yPosition])
+  }, [yPosition, currentPhoto])
 
-  const handleClick = () => {
+  const handleModalClick = () => {
     document.body.classList.remove('modal-open')
     setShowModal(false);
+  }
+
+  const handleLeftButtonClick = () => {
+    if (currentPhoto > 0) {
+      setPreviousPhoto(currentPhoto)
+      setCurrentPhoto(currentPhoto - 1)
+    }
+  }
+
+  const handleRightButtonClick = () => {
+    if (currentPhoto < currentStyle.photos.length - 1) {
+      setPreviousPhoto(currentPhoto)
+      setCurrentPhoto(currentPhoto + 1)
+    }
   }
 
   return ReactDom.createPortal(
     <div id="image-modal-container" className="image-modal-container" ref={imageModalRef}>
       <div className="image-modal">
+        <div className="modal-buttons-container">
+          { currentPhoto === 0 ? null :
+            <button
+              className="modal-image left"
+              onClick={() => handleLeftButtonClick()}>
+            </button>
+          }
+          { currentPhoto === currentStyle.photos.length - 1 ? null :
+            <button
+              className="modal-image right"
+              onClick={() => handleRightButtonClick()}>
+            </button>
+          }
+        </div>
         <img ref={imageRef}
           src={currentStyle.photos[currentPhoto].url}
-          onClick={() => handleClick()} className="image-modal-image"
+          onClick={() => handleModalClick()} className="image-modal-image"
           onMouseMove={(e) => handlePosition(e)}
           style = {{top}}
         />
