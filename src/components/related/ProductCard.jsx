55658d163c477d7baf00330cf84eee114d412854
Calvin See <calvinfrancissee@gmail.com>
2022-04-11 13:24:00 -0700

    Cleaned up ProductCard code

--- src/components/related/ProductCard.jsx
+++ src/components/related/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useEffect, useState} from 'react';
+import React, {useMemo, useEffect, useState} from 'react';
 import {CompareButton, RemoveButton} from './ActionButtons.jsx';
 import {getAverageRating} from './RelatedHelpers.js';
 import StarRating from './StarRating.jsx';
@@ -6,58 +6,43 @@ import {getProduct, getStyles, getReviewMetadata} from '../../helpers.js';
 
 
 const ProductCard = React.memo(function ProductCard({product_id, related}) {
-  const productInfo = useRef(null);
-  const styleData = useRef(null);
-  const reviewData = useRef(null);
-  const [loadState, setLoadState] = useState(false);
+  const [cardData, setCardData] = useState({});
 
   useEffect(() => {
     getProduct(product_id)
-      .then((product) => {
-        productInfo.current = {category: product.category, name: product.name, features: product.features};
-        return getStyles(product_id);
-      })
-      .then((style) => {
-        styleData.current = {
-          image: style[0].photos[0].thumbnail_url,
-          salePrice: style[0].sale_price,
-          originalPrice: style[0].original_price
-        };
-        return getReviewMetadata(product_id);
-      })
-      .then((reviews) => {
-        reviewData.current = reviews.ratings;
-        setLoadState(true);
-      })
-      .catch((err) => {
-        console.error(err);
-        setLoadState(false);
-      });
+      .then((productData) => getStyles(product_id)
+        .then((styleData) => getReviewMetadata(product_id)
+          .then((reviewData) => {
+            setCardData({productData, styleData, reviewData: reviewData.ratings});
+          })))
+      .catch((err) => console.error(err));
+
+
   }, [product_id]);
 
-  const averageRating = useMemo(() => getAverageRating(reviewData.current), [reviewData.current]);
+  const averageRating = useMemo(() => getAverageRating(cardData.reviewData), [cardData]);
 
   return (
     <div className="product-card">
-      {loadState ?
+      {cardData.productData ?
       <React.Fragment>
         <div className="card-top">
-        {related ? <CompareButton cardData={productInfo.current} /> :
+        {related ? <CompareButton cardData={cardData.productData} /> :
         <RemoveButton />}
-        {!styleData.current.image ?
-        <div className="related-image no-image">NO IMAGE</div> :
-        <img className="related-image" src={styleData.current.image} />
+        {cardData.styleData[0].photos[0].thumbnail_url ?
+        <img className="related-image" src={cardData.styleData[0].photos[0].thumbnail_url} /> :
+        <div className="related-image no-image">NO IMAGE</div>
         }
       </div>
       <div className="card-bot">
-        <section className="body-text">{productInfo.current.category}</section>
-        <section className="related-name"><h2>{productInfo.current.name}</h2></section>
-        {styleData.current.salePrice ?
+        <section className="body-text">{cardData.productData.category}</section>
+        <section className="related-name"><h2>{cardData.productData.name}</h2></section>
+        {cardData.styleData.sale_price ?
           <div className="sale-price-container">
-            <section className="body-text sale-price">{styleData.current.salePrice} USD</section>
-            <section className="body-text original-price">{styleData.current.originalPrice} USD</section>
+            <section className="body-text sale-price">{cardData.styleData.sale_price} USD</section>
+            <section className="body-text original-price">{cardData.styleData.original_price} USD</section>
           </div> :
-          <section className="body-text price">{styleData.current.originalPrice} USD</section>
+          <section className="body-text price">{cardData.styleData.original_price} USD</section>
         }
         <div className="average-star-container product-stars">
           {averageRating > 0 ? <StarRating averageRating={averageRating} /> : null}
