288e4c305baffddfbb8b79b047d8c02c0cfd7108
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-12 14:14:01 -0700

    Refactored overview component to render on product data change!

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -3,35 +3,26 @@ import ImageCarousel from './ImageCarousel.jsx';
 import ImageBar from "./ImageBar.jsx";
 import ImageModal from "./ImageModal.jsx";
 import ProductInformation from './ProductInformation.jsx';
-import { getProduct, getStyles } from "../../helpers.js";
+import { getStyles } from "../../helpers.js";
 import { AppContext } from "../App.jsx";
 
 export const OverviewContext = createContext({});
 
 const Overview = () => {
 
-  const [product, setProduct] = useState({});
   const [styles, setStyles] = useState([]);
   const [currentStyle, setCurrentStyle] = useState({});
   const [currentSize, setCurrentSize] = useState('');
   const [quantity, setQuantity] = useState(1);
   const [currentPhoto, setCurrentPhoto] = useState(0);
   const [previousPhoto, setPreviousPhoto] = useState(0);
-  const [progress, setProgress] = useState(0);
   const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(true);
 
-  const { productId } = useContext(AppContext);
+  const { productId, productData } = useContext(AppContext);
 
   const getProductData = () => {
-    getProduct(productId)
-      .then((response) => {
-        setProduct(response);
-        return response.id;
-      })
-      .then((response) => {
-        return getStyles(response);
-      })
+    getStyles(productData.id)
       .then((styleData) => {
         setStyles(styleData);
         return styleData;
@@ -45,9 +36,6 @@ const Overview = () => {
           }
         }
       })
-      .then((style) => {
-        setProgress(1 / style.photos.length)
-      })
       .then(() => {
         setLoading(false);
       })
@@ -58,10 +46,10 @@ const Overview = () => {
 
   useEffect(() => {
     getProductData();
-  }, [productId]);
+  }, [productData.id]);
 
   return (
-    <OverviewContext.Provider value={{ product, productId, styles, currentStyle, setCurrentStyle, currentSize, setCurrentSize, quantity, setQuantity, currentPhoto, setCurrentPhoto, previousPhoto, setPreviousPhoto, progress, setProgress, showModal, setShowModal, loading }}>
+    <OverviewContext.Provider value={{ productId, styles, currentStyle, setCurrentStyle, currentSize, setCurrentSize, quantity, setQuantity, currentPhoto, setCurrentPhoto, previousPhoto, setPreviousPhoto, showModal, setShowModal, loading }}>
       <div className="overview">
         {showModal ? <ImageModal /> : null}
         <div className="overview-container">
--- src/components/overview/ProductInformation.jsx
+++ src/components/overview/ProductInformation.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from "react";
 import StyleSelector from './StyleSelector.jsx';
 import SizeSelector from './SizeSelector.jsx';
 import QuantitySelector from './QuantitySelector.jsx';
-import { OverviewContext } from "./Overview.jsx";
+import { OverviewContext } from './Overview.jsx';
+import { AppContext } from '../App.jsx';
 import { postCart } from '../../helpers.js';
 import { SiFacebook, SiTwitter, SiPinterest, SiInstagram } from 'react-icons/si';
 
 const ProductInformation = () => {
 
-  const { product, currentStyle, currentSize, setCurrentSize, loading } = useContext(OverviewContext);
+  const { currentStyle, currentSize, setCurrentSize, loading } = useContext(OverviewContext);
+  const { productData } = useContext(AppContext);
 
   if (loading) {
     return <div className="overview-product-info loading"></div>
@@ -21,14 +23,14 @@ const ProductInformation = () => {
 
   return (
     <div className="overview-product-info">
-      <h1>{product.name}</h1>
-      <section className="body-text">{product.description}</section>
+      <h1>{productData.name}</h1>
+      <section className="body-text">{productData.description}</section>
       { currentStyle.sale_price ?
       <div className="sale-price-container">
         <section className="body-text sale-price">{currentStyle.sale_price} USD</section>
-        <section className="body-text original-price">{product.default_price} USD</section>
+        <section className="body-text original-price">{productData.default_price} USD</section>
       </div> :
-        <section className="body-text price">{product.default_price} USD</section>
+        <section className="body-text price">{productData.default_price} USD</section>
       }
       <div className="social-media-container">
         <a
