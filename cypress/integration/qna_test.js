f41bd6cb09285c955cc6e89189a4d3f59c968da5
Andrew Tran <amtran150@gmail.com>
2022-04-12 22:32:17 -0700

    began building tests for widget

--- cypress/integration/qna_test.js
+++ cypress/integration/qna_test.js
@@ -1,7 +1,77 @@
-describe('My First Test', () => {
-  it('Visits localhost', () => {
+describe('Questions and Answers', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+      .wait(2000);
+  });
 
-    cy.contains('Questions and Answers')
+  it ('Finds Questions and Answers Widget', () => {
+    cy.contains('QUESTIONS AND ANSWERS');
+    cy.get('.qna-container')
   })
-})
+
+  it ('Finds AddQuestionModal', () => {
+    cy.get('.questions-container')
+      .find('.add-question').click()
+      .get('.add-question-container')
+      .find('.question-modal')
+      .should('exist');
+  })
+
+  it ('Clicking AddAnswer should open modal', () => {
+    cy.get('.question-head')
+      .get('.helpful')
+      .eq(2)
+      .click()
+      .get('.question-modal')
+      .should('exist');
+  })
+
+  it ('Typing in searchbar should update list', () => {
+    cy.get('.search-bar')
+      .type('lkjferandom')
+      .get('.questions-list')
+      .get('.question-head')
+      .should('not.exist')
+  })
+
+  // context('Opens a Comparison Modal on Action Button Click', () => {
+  //   beforeEach(() => {
+  //     cy.get('#related-list')
+  //     .find('.product-card:first-of-type')
+  //     .find('.compare-button')
+  //     .click();
+  //   });
+
+  //   it('Finds a Comparison Modal', () => {
+  //     cy.get('.compare-modal-container');
+  //   });
+  // });
+
+  // context('Outfit List', () => {
+  //   it('Finds a Product Card in Your Outfit List', () => {
+  //     cy.get('#outfit-list')
+  //       .find('#add-card')
+  //       .click()
+  //       .get('#outfit-list')
+  //       .find('.product-card')
+  //       .not('#add-card');
+  //   });
+
+  //   it('Should find a remove-button and click it', () => {
+  //     cy.get('#outfit-list')
+  //       .find('#add-card')
+  //       .click();
+
+
+  //     cy.get('#outfit-list')
+  //       .find('.product-card')
+  //       .not('#add-card')
+  //       .find('.remove-button')
+  //       .click()
+  //       .get('#outfit-list')
+  //       .find('.product-card')
+  //       .not('#add-card')
+  //       .should('not.exist');
+  //   });
+  // });
+});
