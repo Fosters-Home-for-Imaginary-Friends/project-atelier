3e3a6cdf8a50b949b99bf8b88b731d9bf520fb90
Calvin See <calvinfrancissee@gmail.com>
2022-04-12 17:00:02 -0700

    Created Comparison Modal Tests

--- cypress/integration/related-products-test.js
+++ cypress/integration/related-products-test.js
@@ -6,4 +6,22 @@ describe('Product Cards', () => {
   it('Finds Product Cards', () => {
     cy.get('.product-card');
   });
+
+  context('Opens a Comparison Modal on Action Button Click', () => {
+    beforeEach(() => {
+      cy.get('#related-list')
+      .find('.product-card:first-of-type')
+      .find('.compare-button')
+      .click();
+    });
+
+    it('Finds a Comparison Modal', () => {
+      cy.get('.compare-modal-container');
+    });
+  });
+  context('Adds a Product Card to Your Outfit List', () => {
+    beforeEach(() => {
+
+    });
+  });
 });
