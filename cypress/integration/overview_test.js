541a984a5f80e842646b3486ef22b3b66aff4a55
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-12 19:39:51 -0700

    Added a little bit of functionality to overview testing!

--- cypress/integration/overview_test.js
+++ cypress/integration/overview_test.js
@@ -1,23 +1,12 @@
-// import { describe, it, cy } from "cypress";
-
 describe('Overview Test Suite', () => {
-  // beforeEach(() => {
-  //   cy.visit('http://localhost:3000')
-  // })
-
-  // it('Visits Atelier website', () => {
-  //   cy.visit('http://localhost:3000')
-  // })
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+      .wait(1000)
+  })
 
   it('Contains a button that is clickable', () => {
     cy.visit('http://localhost:3000')
 
     cy.get('.info-button.add').click()
   })
-
-  // it('Renders the sale price if current style has a sale price', () => {
-  //   cy.visit('http://localhost:3000')
-
-
-  // })
 })
