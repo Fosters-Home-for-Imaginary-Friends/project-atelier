25b85c4c951075707334977ad2a22fd2bfc128e3
Calvin See <calvinfrancissee@gmail.com>
2022-04-14 11:55:36 -0700

    Optimized webpack config

--- server/index.js
+++ server/index.js
@@ -3,5 +3,6 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+
 app.use(express.static(path.join(__dirname, '/../public')));
 app.listen(port, () => {console.log('listening on port: ', 3000)});
