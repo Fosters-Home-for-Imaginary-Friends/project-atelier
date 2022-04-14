aa7887bb6d8c5330a679b74d97d73796de9be843
Calvin See <calvinfrancissee@gmail.com>
2022-04-14 10:59:17 -0700

    Added express server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname, '/../public')));
+app.listen(port, () => {console.log('listening on port: ', 3000)});
