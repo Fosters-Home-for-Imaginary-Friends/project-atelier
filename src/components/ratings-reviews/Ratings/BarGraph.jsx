15800ec76a581b5d985807c958b73749ba6d2853
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-04-16 11:09:44 -0700

    Progress on barGraph and characteristic refactor!

--- src/components/ratings-reviews/Ratings/BarGraph.jsx
+++ src/components/ratings-reviews/Ratings/BarGraph.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import { RatingsContext } from '../Ratings.jsx';
 
 let BarGraph = (props) => {
-  const {metaRating, totalReviews, filteredContent} = useContext(RatingsContext);
+
+  const {metaRating, totalReviews, filteredContent, loading} = useContext(RatingsContext);
 
   //var declarations to set up % for colored bar graph portion.
   //total of all percents should equal 100
@@ -17,46 +18,45 @@ let BarGraph = (props) => {
     filteredContent(e.target.id);
   }
 
+  if (loading) {
+    return <div className="bar-graph-container loading"></div>
+  }
+
   return (
     <div className="bar-graph-container">
       <div className="graph-title-bar">
         <section id="5" className="bar-titles body-text" onClick={filterClick}>5 Stars</section>
         <div className="star-bar">
-          <div className="bar"></div>
-          <div className="bar scored-bar" style={{"width": `${fiveStarsPercent * 20}vw`}}></div>
-          <span className="helpful-answer" style={{"transform": `translateX(calc(${fiveStarsPercent * 20}vw + 15px))`}}> {ratingsObj[5]} </span>
+            <div className="bar scored-bar" style={{"width": `${fiveStarsPercent * 100}%`}}></div>
+            <span className="helpful-answer scored-bar-label"> {ratingsObj[5]} </span>
         </div>
       </div>
       <div className="graph-title-bar">
         <section id="4" className="bar-titles body-text" onClick={filterClick}>4 Stars</section>
         <div className="star-bar">
-          <div className="bar"></div>
-          <div className="bar scored-bar" style={{"width": `${fourStarsPercent * 20}vw`}}></div>
-          <span className="helpful-answer" style={{"transform": `translateX(calc(${fourStarsPercent * 20}vw + 15px))`}}> {ratingsObj[4]} </span>
+          <div className="bar scored-bar" style={{"width": `${fourStarsPercent * 100}%`}}></div>
+          <span className="helpful-answer scored-bar-label"> {ratingsObj[4]} </span>
         </div>
       </div>
       <div className="graph-title-bar">
         <section id="3"className="bar-titles body-text" onClick={filterClick}>3 Stars</section>
         <div className="star-bar">
-          <div className="bar"></div>
-          <div className="bar scored-bar" style={{"width": `${threeStarsPercent * 20}vw`}}></div>
-        <span className="helpful-answer" style={{"transform": `translateX(calc(${threeStarsPercent * 20}vw + 15px))`}}> {ratingsObj[3]} </span>
+          <div className="bar scored-bar" style={{"width": `${threeStarsPercent * 100}%`}}></div>
+        <span className="helpful-answer scored-bar-label"> {ratingsObj[3]} </span>
         </div>
       </div>
       <div className="graph-title-bar">
         <section id="2" className="bar-titles body-text" onClick={filterClick}>2 Stars</section>
         <div className="star-bar">
-          <div className="bar"></div>
-          <div className="bar scored-bar" style={{"width": `${twoStarsPercent * 20}vw`}}></div>
-          <span className="helpful-answer" style={{"transform": `translateX(calc(${twoStarsPercent * 20}vw + 15px))`}}> {ratingsObj[2]} </span>
+          <div className="bar scored-bar" style={{"width": `${twoStarsPercent * 100}%`}}></div>
+          <span className="helpful-answer scored-bar-label"> {ratingsObj[2]} </span>
         </div>
       </div>
       <div id="one-star-graph" className="graph-title-bar">
         <section id ="1" className="bar-titles body-text" onClick={filterClick}>1 Stars</section>
         <div className="star-bar">
-          <div className="bar" ></div>
-          <div className="bar scored-bar" style={{"width": `${oneStarsPercent * 20}vw`}}></div>
-          <span className="helpful-answer" style={{"transform": `translateX(calc(${oneStarsPercent * 20}vw + 15px))`}}> {ratingsObj[1]} </span>
+          <div className="bar scored-bar" style={{"width": `${oneStarsPercent * 100}%`}}></div>
+          <span className="helpful-answer scored-bar-label"> {ratingsObj[1]} </span>
         </div>
       </div>
     </div>
--- src/components/ratings-reviews/Ratings/IndividualCharacteristic.jsx
+++ src/components/ratings-reviews/Ratings/IndividualCharacteristic.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
 import { GoTriangleDown } from 'react-icons/go';
+import { RatingsContext } from '../Ratings.jsx';
 
 let IndividualCharacteristic = (props) => {
+
+  const charRef = useRef(null);
+
+  const { loading } = useContext(RatingsContext);
+
   let char = props.char;
+
+  if (char.Fit) {
+    console.log(char.Fit.value);
+  }
+
+  if (!loading) {
+    console.log(charRef.current);
+  }
+
   return (
-  <div className="individual-characterstic-container">
+  <div ref={charRef} className="individual-characterstic-container">
     {(char.Size) &&
     <div className="characteristic-container">
       <section className="body-text"> Size </section>
       <div className="characteristic-bars-container">
         <div className="char-bar"></div>
-        <div className="char-bar"> </div>
-        <div className="char-bar"> </div>
+        <div className="char-bar"></div>
+        <div className="char-bar"></div>
         <GoTriangleDown className="marker" style={{"transform" : `translate(${ char.Size.value * 4.8}vw)`}}/>
       </div>
       <div className="qualities-container">
@@ -26,8 +41,8 @@ let IndividualCharacteristic = (props) => {
       <section className="body-text"> Fit </section>
       <div className="characteristic-bars-container">
         <div className="char-bar"></div>
-        <div className="char-bar"> </div>
-        <div className="char-bar"> </div>
+        <div className="char-bar"></div>
+        <div className="char-bar"></div>
         <GoTriangleDown className="marker" style={{"transform" : `translate(${ char.Fit.value * 4.8}vw)`}}/>
       </div>
       <div className="qualities-container">
@@ -42,8 +57,8 @@ let IndividualCharacteristic = (props) => {
       <section className="body-text"> Width </section>
       <div className="characteristic-bars-container">
         <div className="char-bar"></div>
-        <div className="char-bar"> </div>
-        <div className="char-bar"> </div>
+        <div className="char-bar"></div>
+        <div className="char-bar"></div>
         <GoTriangleDown className="marker" style={{"transform" : `translate(${ char.Width.value * 4.8}vw)`}}/>
       </div>
       <div className="qualities-container">
@@ -58,9 +73,12 @@ let IndividualCharacteristic = (props) => {
       <section className="body-text"> Comfort </section>
       <div className="characteristic-bars-container">
         <div className="char-bar"></div>
-        <div className="char-bar"> </div>
-        <div className="char-bar"> </div>
-        <GoTriangleDown className="marker" style={{"transform" : `translate(${ char.Comfort.value * 4.8}vw)`}}/>
+        <div className="char-bar"></div>
+        <div className="char-bar"></div>
+        <div className="marker-container">
+          <div className="marker-spacer" style={{"width": `${((char.Comfort.value - 1) / 4) * 100}%`}}></div>
+          <GoTriangleDown className="marker"/>
+          </div>
       </div>
       <div className="qualities-container">
         <span className="char-qualities helpful-answer">Uncomfortable</span>
@@ -74,9 +92,12 @@ let IndividualCharacteristic = (props) => {
       <section className="body-text"> Quality </section>
       <div className="characteristic-bars-container">
         <div className="char-bar "></div>
-        <div className="char-bar"> </div>
-        <div className="char-bar"> </div>
-        <GoTriangleDown className="marker" style={{"transform" : `translate(${ char.Quality.value * 4.8}vw)`}}/>
+        <div className="char-bar"></div>
+        <div className="char-bar"></div>
+        <div className="marker-container">
+          <div className="marker-spacer" style={{"width": `${((char.Quality.value - 1) / 4) * 100}%`}}></div>
+          <GoTriangleDown className="marker"/>
+        </div>
       </div>
       <div className="qualities-container">
         <span className="char-qualities helpful-answer">Poor</span>
@@ -90,9 +111,12 @@ let IndividualCharacteristic = (props) => {
       <section className="body-text"> Length </section>
       <div className="characteristic-bars-container">
         <div className="char-bar"></div>
-        <div className="char-bar"> </div>
-        <div className="char-bar"> </div>
-        <GoTriangleDown className="marker" style={{"transform" : `translate(${ char.Length.value * 4.8}vw)`}}/>
+        <div className="char-bar"></div>
+        <div className="char-bar"></div>
+        <div className="marker-container">
+          <div className="marker-spacer" style={{"width": `${((char.Length.value - 1) / 4) * 100}%`}}></div>
+        <GoTriangleDown className="marker"/>
+        </div>
       </div>
       <div className="qualities-container">
         <span className="char-qualities helpful-answer">Too Short</span>
